cd083ecac446beb86f5ade2c8b01008440842a7c
WorldwideErrors <45656346+WorldwideErrors@users.noreply.github.com>
2019-03-19 10:41:20 +0100

    Update PortfolioPage2.php

--- PortfolioPage2.php
+++ PortfolioPage2.php
@@ -179,8 +179,8 @@ if(isset($_POST["btnSave"]))
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'contact.jeffreyvantillo@gmail.com';                 // SMTP username
-        $mail->Password = '14mrt2001';                           // SMTP password
+        $mail->Username = 'Mail@Example.com';                 // SMTP username
+        $mail->Password = 'ExamplePassword';                           // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                    // TCP port to connect to
 
